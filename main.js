8359f035c3c158cd0b460ae5622521ca1314801a
Platon172 <107851230+Platon172@users.noreply.github.com>
2023-03-02 16:26:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let a = prompt();
+let b = prompt ();
+let n1 = Number(a);
+let n2 = Number(b);
+let t = n1 + n2;
+if (t>10) {
+    alert("okay")
+}
+
+let name = prompt();
+if (name = "Platon"){
+    alert("Hello")
+}
+
+else {
+    console.log ("Who are you")
+}
